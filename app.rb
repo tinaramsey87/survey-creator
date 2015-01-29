9b05e59437c137845cab724e67975fbeca136756
Tina Ramsey & Karvari Ellingson <student@epicodus.com>
2015-01-28 17:00:37 -0800

    add questions to survey

--- app.rb
+++ app.rb
@@ -25,7 +25,8 @@ get('/taker/survey') do
 end
 
 get('/designer/survey/:id/edit') do
-  @survey = Survey.find(params["id"].to_i)
+  @survey = Survey.find(params["id"].to_i())
+  @questions = Question.all
   erb(:survey_edit)
 end
 
@@ -39,5 +40,31 @@ end
 delete('/designer/survey/:id') do
   survey = Survey.find(params.fetch("id").to_i())
   survey.destroy
-  redirect("designer/survey")
+  redirect("/designer/survey")
+end
+
+post('/designer/survey/:id') do
+  @survey = Survey.find(params.fetch("id").to_i())
+  @questions = Question.all
+  question = params['question']
+  choices = params['choices_number'].to_s
+  @question = Question.create( {:description => question, :type => choices })
+  erb(:survey_edit)
+end
+
+get("/question/:id/edit") do
+  @questions = Question.find(params.fetch("id").to_i())
+  erb(:question_edit)
+end
+#
+# get("/designer/question/:id")do
+#   question = Question.find(params.fetch("id").to_i())
+#   redirect("/designer/survey/:id/edit")
+# end
+
+delete("/designer/question/:id") do
+  @survey = Survey.find(params.fetch("id").to_i())
+  question = Question.find(params.fetch("id").to_i())
+  question.destroy
+  redirect back
 end
